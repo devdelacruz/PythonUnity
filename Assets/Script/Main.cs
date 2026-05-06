4c068df538f615a1ca71b0db9cb3c497589b9293
AngelKeena <angelkeenadelacruz@gmail.com>
2026-05-06 20:48:18 +0800

    Upgrade Buttons,Bg Sprite,TitleSprite

--- Assets/Script/Main.cs
+++ Assets/Script/Main.cs
@@ -7,11 +7,40 @@ public class Main : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI incomeText;
+
+    public TextMeshProUGUI toppingsLevelText;
+    public TextMeshProUGUI signLevelText;
+    public TextMeshProUGUI scrambleMakerLevelText;
+    public TextMeshProUGUI foodcartLevelText;
+    public TextMeshProUGUI decorationsLevelText;
+    public TextMeshProUGUI scrambleTropaLevelText;
+
     public string scoreString = "₱";
     public double score = 0;
     public double incomePerSecond = 0.1;
 
+    private int toppingsLevel = 0;
+    private int signLevel = 0;
+    private int scrambleMakerLevel = 0;
+    private int foodcartLevel = 0;
+    private int decorationsLevel = 0;
+    private int scrambleTropaLevel = 0;
+
+    private int maxUpgradeLevel = 10;
+
+    private double toppingsUpgradeAmount = 0.5;
+    private double signUpgradeAmount = 1.0;
+    private double scrambleMakerUpgradeAmount = 2.0;
+    private double foodcartUpgradeAmount = 4.0;
+    private double decorationsUpgradeAmount = 8.0;
+    private double scrambleTropaUpgradeAmount = 15.0;
+
     float timer = 0f;
+
+    void Start()
+    {
+        ForceUIUpdate();
+    }
 
     void Update()
     {
@@ -22,54 +51,149 @@ public class Main : MonoBehaviour
             score += incomePerSecond;
             timer -= 1f;
         }
-        scoreText.text = score.ToString();
+
+        scoreText.text = score.ToString("0.0");
     }
 
     public void ForceReset()
     {
         score = 0;
-        incomePerSecond = 0.1;
-        ForceUIUpdate();
+        incomePerSecond = 1.0;
 
-        //Reset upgrades
-    }
+        toppingsLevel = 0;
+        signLevel = 0;
+        scrambleMakerLevel = 0;
+        foodcartLevel = 0;
+        decorationsLevel = 0;
+        scrambleTropaLevel = 0;
 
-    //void OnEnable()
-    //{
-    //    ForceReset();
-    //}
-
-    void Start()
-    {
         ForceUIUpdate();
     }
 
     public void ForceUIUpdate()
     {
-        scoreString += scoreString.ToString();
-        incomeText.text = incomePerSecond.ToString();
-    }
+        incomeText.text = incomePerSecond.ToString("0.0");
 
-    public void UpgradeScore()
-    {
-        incomePerSecond += 0.1;
-
-        incomeText.text = incomePerSecond.ToString();
+        ToppingsLevelDisplay();
+        SignLevelDisplay();
+        ScrambleMakerLevelDisplay();
+        FoodcartLevelDisplay();
+        DecorationsLevelDisplay();
+        ScrambleTropaLevelDisplay();
     }
 
     public void UpdateScore()
     {
-        double scoreCurr = score + 0.1;
-        score = scoreCurr;
+        score += 1.0;
+        incomeText.text = incomePerSecond.ToString("0.0");
+    }
 
-        // Raw print value as a string
-        //scoreText.text = scoreCurr.ToString();
-        //incomeText.text = incomePerSecond.ToString();
+    public void UpgradeToppings()
+    {
+        if (toppingsLevel >= maxUpgradeLevel)
+        {
+            return;
+        }
 
-        // Add ₱ as a string
-        scoreString += scoreCurr.ToString();
-        incomeText.text = incomePerSecond.ToString();
+        toppingsLevel++;
+        incomePerSecond += toppingsUpgradeAmount;
 
+        ForceUIUpdate();
+    }
+
+    public void UpgradeSign()
+    {
+        if (signLevel >= maxUpgradeLevel)
+        {
+            return;
+        }
+
+        signLevel++;
+        incomePerSecond += signUpgradeAmount;
+
+        ForceUIUpdate();
+    }
+
+    public void UpgradeScrambleMaker()
+    {
+        if (scrambleMakerLevel >= maxUpgradeLevel)
+        {
+            return;
+        }
+
+        scrambleMakerLevel++;
+        incomePerSecond += scrambleMakerUpgradeAmount;
+
+        ForceUIUpdate();
+    }
+
+    public void UpgradeFoodcart()
+    {
+        if (foodcartLevel >= maxUpgradeLevel)
+        {
+            return;
+        }
+
+        foodcartLevel++;
+        incomePerSecond += foodcartUpgradeAmount;
+
+        ForceUIUpdate();
+    }
+
+    public void UpgradeDecorations()
+    {
+        if (decorationsLevel >= maxUpgradeLevel)
+        {
+            return;
+        }
+
+        decorationsLevel++;
+        incomePerSecond += decorationsUpgradeAmount;
+
+        ForceUIUpdate();
+    }
+
+    public void UpgradeScrambleTropa()
+    {
+        if (scrambleTropaLevel >= maxUpgradeLevel)
+        {
+            return;
+        }
+
+        scrambleTropaLevel++;
+        incomePerSecond += scrambleTropaUpgradeAmount;
+
+        ForceUIUpdate();
+    }
+
+    public void ToppingsLevelDisplay()
+    {
+        toppingsLevelText.text = toppingsLevel + "/" + maxUpgradeLevel;
+    }
+
+    public void SignLevelDisplay()
+    {
+        signLevelText.text = signLevel + "/" + maxUpgradeLevel;
+    }
+
+    public void ScrambleMakerLevelDisplay()
+    {
+        scrambleMakerLevelText.text = scrambleMakerLevel + "/" + maxUpgradeLevel;
+    }
+
+    public void FoodcartLevelDisplay()
+    {
+        foodcartLevelText.text = foodcartLevel + "/" + maxUpgradeLevel;
+    }
+
+    public void DecorationsLevelDisplay()
+    {
+        decorationsLevelText.text = decorationsLevel + "/" + maxUpgradeLevel;
+    }
+
+    public void ScrambleTropaLevelDisplay()
+    {
+        scrambleTropaLevelText.text = scrambleTropaLevel + "/" + maxUpgradeLevel;
     }
 
     public static void SavePlayerPython()
@@ -79,7 +203,6 @@ public class Main : MonoBehaviour
 
     public void SavePlayer()
     {
-        // C#
         PlayerData data = new PlayerData(score, incomePerSecond);
 
         string json = JsonUtility.ToJson(data, true);
@@ -88,14 +211,10 @@ public class Main : MonoBehaviour
         File.WriteAllText(path, json);
 
         Debug.Log("Saved to: " + path);
-
-        // Run Python SaveData
-        // SavePlayerPython();
     }
 
     public void LoadPlayer()
     {
-        // Identify JSON path
         string path = Application.persistentDataPath + "/player.json";
 
         if (File.Exists(path))
@@ -104,14 +223,13 @@ public class Main : MonoBehaviour
 
             PlayerData data = JsonUtility.FromJson<PlayerData>(json);
 
-            Debug.Log("Name: " + data.sc);
-            Debug.Log("Level: " + data.ic);
+            Debug.Log("Score: " + data.sc);
+            Debug.Log("Income: " + data.ic);
 
             score = data.sc;
             incomePerSecond = data.ic;
 
-            scoreString += data.sc.ToString();
-            incomeText.text = data.ic.ToString();
+            ForceUIUpdate();
         }
         else
         {
